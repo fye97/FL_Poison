395d6888d1a8ca63fa6eeb7debede4a56b52baf1
puyanguvic <npuyangpu@gmail.com>
2026-02-04 18:09:03 -0800

    add an experiment bash

---
+++ exps/exp1_fang_fedsgd_mnist.sh
@@ -0,0 +1,5 @@
+python main.py -config=./configs/FedSGD_MNIST_config.yaml -attack=MinSum -defense=Mean
+
+python main.py -config=./configs/FedSGD_MNIST_config.yaml -attack=MinSum -defense=MultiKrum
+
+python main.py -config=./configs/FedSGD_MNIST_config.yaml -attack=MinSum -defense=SigGuard
